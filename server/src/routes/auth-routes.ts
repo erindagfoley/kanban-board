59f1aa9bdabb44f1ab76ed6dfade5a96d68f326a
erin foley <erindagfoley@gmail.com>
2025-02-13 10:31:22 -0500

    starter code again

--- server/src/routes/auth-routes.ts
+++ server/src/routes/auth-routes.ts
@@ -5,26 +5,7 @@ import bcrypt from 'bcrypt';
 
 export const login = async (req: Request, res: Response) => {
   // TODO: If the user exists and the password is correct, return a JWT token
-  const { username, password } = req.body;
-
-  const user = await User.findOne({-
-    where: { username },
-  });
-
-  if (!username) {
-    return res.status(401).json({ message: 'Authentication failed' });
-  }
-
-  const passwordIsValid = await bcrypt.compare(password);
-  if (!passwordIsValid) {
-    return res.status(401).json({ message: 'Authentication failed' });
-  }
-
-  const secretKey = process.env.JWT_SECRET_KEY || '';
-
-  const token = jwt.sign({ username }, secretKey, { expiresIn: '1h' });
-
-  return res.json({ token });
+  User.findOne
 };
 
 const router = Router();
